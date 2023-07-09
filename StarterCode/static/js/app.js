6ee0cc6a2d687b491ccb7764097aea29af5846e7
Portia17 <popr2001@colorado.edu>
2023-07-09 15:47:32 -0600

    Completed project base, might do more later with gauge

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -97,6 +97,36 @@ function makeBubbleChart(sample_filter) {
 
 }
 
+// function makeGauge(sample_filter) {
+//     d3.json(url).then((data) => {
+//         console.log(data);
+//         let samples1 = data.samples;
+//         console.log("samples: ->", samples1);
+//         let sampleArray1 = samples1.filter(value => value.id == sample_filter)
+//         console.log("filtered! ->", sampleArray1);
+//         let data_result = sampleArray1[0]
+//         console.log("finally!! ->", data_result);
+        
+
+//         let otu_ids = data_result.otu_ids;
+//         let otu_labels = data_result.otu_labels;
+//         let sample_values = data_result.sample_values;
+
+//         var data = [
+//             {
+//                 domain: { x: [0, 1], y: [0, 1] },
+//                 value: 270,
+//                 title: { text: "Speed" },
+//                 type: "indicator",
+//                 mode: "gauge+number"
+//             }
+//         ];
+        
+//         var layout = { width: 600, height: 500, margin: { t: 0, b: 0 } };
+//         Plotly.newPlot('myDiv', data, layout);
+//     });
+// }
+
 function display(sample_filter) {
     d3.json(url).then((data) => {
         console.log(data);
@@ -104,9 +134,15 @@ function display(sample_filter) {
         let demo_filtered = people.filter(value => value.id == sample_filter)
         console.log("people", people);
         console.log("people2", demo_filtered)
-        demo= document.getElementById("sample-metadata")
+        let demo= Object.entries(demo_filtered[0])
+        d3.selectAll('p').remove()
+        d3.select('#sample-metadata').selectAll('p').data(demo).enter().append('p').text(d => {
+            return `${d[0]}: ${d[1]}`})
+
     });
 }
+
+
 
 function main() {
     let user_selection = d3.select("#selDataset");
